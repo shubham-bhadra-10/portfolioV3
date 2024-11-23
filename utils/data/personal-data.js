ef51449d7187371f2785fedf8b2bd40b2740983f
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2024-11-22 19:43:54 -0500

    Updated Data Resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,16 +1,19 @@
 export const personalData = {
-  name: "SHUBHAM BHADRA",
+  name: 'SHUBHAM BHADRA',
   profile: '/shubhamprofile.png',
-  designation: "Software Engineer",
-  description: "I'm SHUBHAM BHADRA, a dedicated engineer who thrives on challenges and continuous learning. My passion for technology extends to both web development and data science, where I constantly seek to expand my knowledge and expertise. With a proactive approach to learning, I'm always eager to explore emerging technologies and innovative solutions. My enthusiasm for problem-solving drives me to tackle complex tasks with creativity and determination. I'm open to new job opportunities that align with my skills and interests, eager to contribute to projects that make a meaningful impact in the digital world.",
+  designation: 'Software Engineer',
+  description:
+    "I'm SHUBHAM BHADRA, a dedicated engineer who thrives on challenges and continuous learning. My passion for technology extends to both web development and data science, where I constantly seek to expand my knowledge and expertise. With a proactive approach to learning, I'm always eager to explore emerging technologies and innovative solutions. My enthusiasm for problem-solving drives me to tackle complex tasks with creativity and determination. I'm open to new job opportunities that align with my skills and interests, eager to contribute to projects that make a meaningful impact in the digital world.",
   email: 'bhadra.sh@northeastern.edu',
   phone: '+1 857-398-3731',
   address: 'Boston,MA,USA ',
   github: 'https://github.com/shubham-bhadra-10',
   linkedIn: 'https://www.linkedin.com/in/sjkbhadra/',
   leetcode: 'https://leetcode.com/u/user9067bR/',
-  resume: 'https://drive.google.com/file/d/1ZLwtlLVQNxVzLXBWr8xZKPM-RhVg6yF_/view?usp=sharing',
-  resumeDA: 'https://drive.google.com/file/d/1O3smMdAkKBV0MIO_3MsoicMTZzGBWy4L/view?usp=sharing',
+  resume:
+    'https://drive.google.com/file/d/1ZLwtlLVQNxVzLXBWr8xZKPM-RhVg6yF_/view?usp=sharing',
+  resumeDA:
+    'https://docs.google.com/document/d/1f-PO3tFwggpahUyRZXqx7iQF50FfJVcXi6FJgw_L2YE/edit?usp=sharing',
 
-  devUsername: "shubham10"
-}
+  devUsername: 'shubham10',
+};
