14e100a6c492b5175b490317c5f13c4c463787ab
shubham-bhadra-10 <shubhambhadra10@gmail.com>
2025-01-12 12:09:35 -0500

    Updated experience section

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,19 +2,19 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Engineer - System Analyst',
-    company: "HDFC Bank LTD.",
-    duration: "(July 2023 - July 2024)"
+    company: 'HDFC Bank LTD.',
+    duration: '(July 2023 - July 2024)',
   },
   {
     id: 2,
-    title: " Research Assistant",
-    company: "University of Mumbai",
-    duration: "(July 2022 - Jan 2023)"
+    title: ' Research Assistant',
+    company: 'University of Mumbai',
+    duration: '(July 2022 - Jan 2023)',
   },
   {
     id: 3,
-    title: "Data Science intern",
-    company: "University of Mumbai",
-    duration: "(Oct 2020 - Jan 2021)"
-  }
-]
+    title: 'Software Engineer intern',
+    company: 'University of Mumbai',
+    duration: '(Oct 2020 - Jan 2021)',
+  },
+];
